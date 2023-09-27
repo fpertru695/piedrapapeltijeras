178ef35d2d1c9292f1e80af2d13064a97c6013b3
Miguel Pérez <fpertru695@g.educaand.es>
2023-09-27 20:45:27 +0200

    Version 2.0

--- js/main.js
+++ js/main.js
@@ -36,15 +36,18 @@ function determinarResultado(seleccionUsuario, seleccionMaquina) {
   const imagenResultado = seccion5.querySelector("img");
 
   if (seleccionUsuario === seleccionMaquina) {
-    imagenResultado.src = "img/logo.png";
+    playOk();
+    imagenResultado.src = "img/draw.png";
   } else if (
     (seleccionUsuario === "piedra" && seleccionMaquina === "tijera") ||
     (seleccionUsuario === "papel" && seleccionMaquina === "piedra") ||
     (seleccionUsuario === "tijera" && seleccionMaquina === "papel")
   ) {
     imagenResultado.src = "img/win.png";
+    playWin();
   } else {
     imagenResultado.src = "img/lose.png";
+    playLose();
   }
 }
 
@@ -61,7 +64,7 @@ function actualizarPantalla(seleccionUsuario, seleccionMaquina, resultado) {
   const imagenResultado = seccion5.querySelector("img");
 
   if (seleccionUsuario === seleccionMaquina) {
-    imagenResultado.src = "img/logo.png";
+    imagenResultado.src = "img/draw.png";
   } else if (
     (seleccionUsuario === "piedra" && seleccionMaquina === "tijera") ||
     (seleccionUsuario === "papel" && seleccionMaquina === "piedra") ||
@@ -71,4 +74,24 @@ function actualizarPantalla(seleccionUsuario, seleccionMaquina, resultado) {
   } else {
     imagenResultado.src = "img/lose.png";
   }
+}
+
+/* Función para que suene fight cuando le das al botón */
+
+function playAudio() {
+  new Audio("sounds/fight.mp3").play();
+}
+/* Función para que suene victoria cuando ganas */
+function playWin(){
+  new Audio("sounds/win.mp3").play();
+}
+
+/* Función para que suene derrota cuando pierdas */
+function playLose(){
+  new Audio("sounds/lose.mp3").play();
+}
+
+/*Función para que suene ok cuando empate*/
+function playOk(){
+  new Audio("sounds/ok.mp3").play();
 }
